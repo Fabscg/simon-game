0852d41fec8eab3e604ce88fc98950214e46ac07
Fabulosa <fabiscg79@gmail.com>
2022-02-13 13:32:41 -0500

    starting file

---
+++ game.js
@@ -0,0 +1,10 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = randomChosenColour;
+
+
+function nextSequence(){
+    const randomChosenColour = randomNumber[buttonColours.length]
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+
+
+}
